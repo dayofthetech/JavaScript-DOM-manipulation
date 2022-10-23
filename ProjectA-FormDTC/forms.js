65afb0575953f5b6d73ec8b61a597615af859e7f
dayofthetech <j.alberto.reyes.r90@gmail.com>
2022-10-23 13:43:18 -0500

    complete basic form format

--- ProjectA-FormDTC/forms.js
+++ ProjectA-FormDTC/forms.js
@@ -1,56 +1,43 @@
-//Below - what this does, as soon as I click on the input field, it logs the id name of that field, for example
-//click on first name, logs first_name in console
-// const messageForm = document.querySelector('#leave_message').addEventListener('click', function(e){
-//     console.log(e.target.getAttribute('id', 'name'));
-// })
-
-
+//main action of button
 const btn = document.getElementById('btn');
 //global ul element
-const messagesDisplay = document.getElementById('messages-box');
+const messagesDisplay = document.getElementById('messages_box');
+
 
 btn.addEventListener('click', function(e){
     e.preventDefault();
 
-    //.value without the parenthesis is a method
-    //with parenthesis is a function and the code wont work
+    //grab the values from each form field
     const firstName = document.getElementById('first_name').value
-    //const lastName = document.getElementById('last_name').value
     const email = document.getElementById('email').value
     const userMessage = document.getElementById('user-message').value
 
-    //console.log works, now I need to append it to the empty field as an li element
-    //console.log won't work outside the function
-    // console.log(firstName);
-    // console.log(lastName);
-    // console.log(email);
-
+    //create the elements that will go inside the html
     const message = document.createElement('li');
-    //this display as <li>a,r,email@email.com</li>
-    message.innerHTML = [firstName, email, userMessage];
-    //messagesDisplay.appendChild(message);
+    const link = document.createElement('a');
+    const span = document.createElement('span');
+
+    //create button to remove the message
+    const removeBtn = document.createElement('button');
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.setAttribute('type', 'button');
+    removeBtn.addEventListener('click', function(){
+        messagesDisplay.removeChild(message);
+    })
+
+    //create button to edit message
+
+    //format how the elements will look like
+    span.innerHTML = ` wrote ${userMessage} `;
+    link.href = email;
+    link.innerHTML = firstName;
+
+    message.append(link);
+    message.append(span);
+    message.append(removeBtn);
     messagesDisplay.append(message);
 
-    //error
-    //console.log(leaveMessage).value;
-
-    //below - undefined
-    //console.log(message[1]);
-
-
-    // let list = document.getElementById('messages-box');
-    // let data = // list of info from form, probably names?
-
-    //getting the form data
-    // data.forEach((item) => {
-    //   // create list element (<li>)
-    // // set innerText of list element
-    // // append to list
-    // });
-
-
-// <ul>
-// <li> <a href="whatever"/> sometexthere <button type="button">clickhere</button> </li>
-// </ul>
+    const messageForm = document.getElementById('leave_message').reset();
 
 })
+
